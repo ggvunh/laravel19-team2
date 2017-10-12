7416d0afa70b78b8db9a9fdcf00883d2c8a012c8
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2017-10-13 02:15:48 +0700

    update_Toastr

---
+++ config/toastr.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'options' => [
+        "progressBar" => true,
+        "closeButton" => true,
+    ]
+];
--- resources/views/admin/admin-master.blade.php
+++ resources/views/admin/admin-master.blade.php
@@ -62,7 +62,7 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="treeview">
-          <a href="{{url('maneger-menu')}}">
+          <a href="{{url('admin/product/manager-menu')}}">
             <i class="fa fa-edit"></i> <span>Menu</span>
             <span class="pull-right-container">
             </span>
