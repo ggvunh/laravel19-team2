e5a1bca11e968ef800df6f59965b211f3af6b572
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2017-10-17 12:55:48 +0700

    update_shoppingcart_17102017

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Cart;
 use App\Product;
+use App\User;
+use App\Bill;
+use App\BillDetail;
+use Session;
+use Auth;
 class CartController extends Controller
 {
     public function addCart($id)
@@ -26,8 +31,34 @@ class CartController extends Controller
         return redirect()->route('home');
     }
 
-    public function checkout()
+    public function getcheckout()
     {
         return view('cart.cart');
+    }
+    public function checkout( Request $req)
+    {
+        $order_address = $req->input('address');
+        $note = $req->input('note');
+        $bill = new Bill();
+        $bill->date_order = date('Y-m-d');
+        $bill->total = (float)\Cart::total();
+        $bill->order_address = $order_address;
+        $bill->note = $note;
+        $bill->status = '0';
+        $bill->user_id = Auth::id();
+        $bill->save();
+
+        foreach(\Cart::content() as $content)
+        {
+            $billdetail = new BillDetail();
+            $billdetail->quantity = $content->qty;
+            $billdetail->bill_id = $bill->id;
+            $billdetail->product_id = $content->qty;
+            $billdetail->unit_price = $content->price;
+            $billdetail->save();
+        }
+        Session::forget('cart');
+        return redirect()->route('home');
+
     }
 }
