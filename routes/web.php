4b9e8cedd503e2eeec397e939c9ecddedc828d13
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2017-10-23 15:34:21 +0700

    pppppp

--- routes/web.php
+++ routes/web.php
@@ -93,6 +93,7 @@ Route::group(['prefix'=>'dat-hang', 'middleware'=>'order'],function(){
 	Route::GET('add-cartproduct/{id}/{name}','CartController@addCartProduct');
 	Route::GET('add-cartsearch/{id}/{name}','CartController@addCartSearch');
 	Route::GET('add-cartviewdetail/{id}/{name}','CartController@addCartviewdetail1');
+	Route::GET('update_qty_cart/{id}/{name}','CartController@update_qty_cart');
 	});
 Route::group(['prefix'=>'account'], function(){
 	Route::get('orderlists', 'AccountController@getBills');
