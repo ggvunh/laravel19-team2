91721ec73824e4350415f93d86c61a9ab24de3e9
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2017-11-03 00:59:39 +0700

    update_pusher_email

--- app/Mail/OrderShipped.php
+++ app/Mail/OrderShipped.php
@@ -6,20 +6,27 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Bill;
 
 class OrderShipped extends Mailable
 {
     use Queueable, SerializesModels;
 
     /**
+     * The order instance.
+     *
+     * @var Order
+     */
+    protected $billtomail;
+
+    /**
      * Create a new message instance.
      *
      * @return void
      */
-
-    public function __construct()
+     public function __construct(Bill $billtomail)
     {
-        //
+        $this->billtomail = $billtomail;
     }
 
     /**
@@ -29,6 +36,12 @@ class OrderShipped extends Mailable
      */
     public function build()
     {
-        return $this->view('mail.mail');
+        return $this->view('mail.mail')
+                   ->with([
+                       'bill_id' => $this->billtomail->id,
+                       'date_order' => $this->billtomail->date_order,
+                       'total' => $this->billtomail->total,
+                       'address' => $this->billtomail->order_address,
+                   ]);
     }
 }
