eb377224bf0ca1931d26ec1927f217fde5af5ae4
phan anh khoa <anhkhoaphan113@gmail.com>
2017-11-06 12:00:57 +0700

    fix_search_general

--- resources/views/admin/admin-master.blade.php
+++ resources/views/admin/admin-master.blade.php
@@ -236,7 +236,7 @@ desired effect
       <!-- search form (Optional) -->
       <form action="{{url('admin/search-general')}}" method="get" class="sidebar-form">
         <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <input type="text" name="q" class="form-control" placeholder="Search order code...">
           <span class="input-group-btn">
               <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
               </button>
